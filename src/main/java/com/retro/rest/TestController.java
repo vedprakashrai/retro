ce84266f2b58aece610e5be7d41a802b5850254f
vedprakashrai <disturbved@gmail.com>
2019-07-19 22:30:18 -0400

    initial
    ini initial commit

---
+++ src/main/java/com/retro/rest/TestController.java
@@ -0,0 +1,15 @@
+package com.retro.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class TestController {
+
+	
+
+	    @GetMapping
+	    public String helloGradle() {
+	        return "Hello Gradle!";
+}
+}
