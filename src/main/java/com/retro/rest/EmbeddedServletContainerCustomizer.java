c3b19cf9bfc0c869b5266bf728ce7d65080b9656
vedprakashrai <disturbved@gmail.com>
2019-07-20 20:39:41 -0400

    port
    port

--- src/main/java/com/retro/rest/EmbeddedServletContainerCustomizer.java
+++ src/main/java/com/retro/rest/EmbeddedServletContainerCustomizer.java
@@ -1,11 +1,13 @@
 package com.retro.rest;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmbeddedServletContainerCustomizer implements
-WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
-	 public void customize(ConfigurableServletWebServerFactory factory) {
+WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+	 public void customize(TomcatServletWebServerFactory factory) {
 		 if(System.getenv("PORT")!=null)
 	        factory.setPort(Integer.parseInt(System.getenv("PORT")));
 	      factory.setContextPath("");
