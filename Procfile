309a851ffcbd7490c80536ffc4204f0e890bff1a
vedprakashrai <disturbved@gmail.com>
2019-07-21 16:39:02 -0400

    server
    server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java  -Dserver.port=$PORT  -jar webapp-runner-*.jar  build/libs/gradlejava.war
+web: java  -Dserver.port=$PORT  -jar  build/libs/gradlejava-0.0.1.jar
--- src/main/java/com/retro/rest/EmbeddedServletContainerCustomizer.java
+++ src/main/java/com/retro/rest/EmbeddedServletContainerCustomizer.java
@@ -1,16 +1,17 @@
-package com.retro.rest;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class EmbeddedServletContainerCustomizer implements
-WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-	 public void customize(TomcatServletWebServerFactory factory) {
-		 if(System.getenv("PORT")!=null)
-	        factory.setPort(Integer.parseInt(System.getenv("PORT")));
-	      factory.setContextPath("");
-	     }
-
-}
+/*
+ * package com.retro.rest;
+ * 
+ * import
+ * org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+ * import org.springframework.boot.web.server.WebServerFactoryCustomizer; import
+ * org.springframework.stereotype.Component;
+ * 
+ * @Component public class EmbeddedServletContainerCustomizer implements
+ * WebServerFactoryCustomizer<TomcatServletWebServerFactory> { public void
+ * customize(TomcatServletWebServerFactory factory) {
+ * if(System.getenv("PORT")!=null)
+ * factory.setPort(Integer.parseInt(System.getenv("PORT")));
+ * factory.setContextPath(""); }
+ * 
+ * }
+ */
